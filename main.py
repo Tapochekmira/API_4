c11bcf9dacbb41fd07892436ceade50aa0ee195b
Tapochekmira <85840326+Tapochekmira@users.noreply.github.com>
2021-11-09 01:13:29 +0300

    daily photo publication

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import requests
 import os
 import datetime
 import telegram
+import time
 from pathlib import Path
 from urllib.parse import urlparse
 from dotenv import load_dotenv
@@ -44,8 +45,9 @@ def fetch_nasa_APOD(directory, numbers_of_pictures, nasa_token):
     }
     nasa_response = requests.get(nasa_url, params=payload)
     nasa_response.raise_for_status()
-    for picture_number in range(numbers_of_pictures):
-        nasa_picture_url = nasa_response.json()[picture_number]['url']
+    nasa_apod_pictures = nasa_response.json()
+    for picture_number, picture in enumerate(nasa_apod_pictures):
+        nasa_picture_url = picture['url']
         download_picture(directory, f'nasa{picture_number}{get_file_extension(nasa_picture_url)}',
                          nasa_picture_url)
 
@@ -58,40 +60,41 @@ def fetch_nasa_EPIC(directory, nasa_token):
     nasa_earth_response = requests.get(nasa_earth_url, params=payload)
     nasa_earth_response.raise_for_status()
     
-    nasa_earth_picture = nasa_earth_response.json()
-    for number in range(len(nasa_earth_picture) // 2):
-        picture_earth_name = nasa_earth_picture[number]['image']
-        picture_earth_date = nasa_earth_picture[number]['date']
+    nasa_earth_pictures = nasa_earth_response.json()
+    for picture_number, picture in enumerate(nasa_earth_pictures):
+        picture_earth_name = picture['image']
+        picture_earth_date = picture['date']
 
         picture_earth_date = datetime.datetime.fromisoformat(picture_earth_date)
         picture_earth_date = picture_earth_date.strftime("%Y/%m/%d")
 
         picture_earth_url = f'https://api.nasa.gov/EPIC/archive/natural/{picture_earth_date}/png/{picture_earth_name}.png'
-        download_picture(directory, f'EPIC{number}.png', picture_earth_url, nasa_token)
+        download_picture(directory, f'EPIC{picture_number}.png', picture_earth_url, nasa_token)
 
 
-def publish_text_to_telegram(telegram_token, directory):
-    all_pictures = os.listdir(directory)
-    
-    
+def publish_text_to_telegram(telegram_token, directory, picture):
     chat_id='@Kosmo_Super_Kek'
     bot = telegram.Bot(token=telegram_token)
-    bot.send_document(chat_id=chat_id, document=open(f'{directory}{all_pictures[0]}', 'rb'))
+    bot.send_document(chat_id=chat_id, document=open(f'{directory}{picture}', 'rb'))
     
     
 if __name__ == '__main__':
     load_dotenv()
     nasa_token = os.environ['NASA_TOKEN']
     telegram_token = os.environ['TELEGRAM_TOKEN']
-
+    sleep_time = os.environ['SLEEP_TIME']
+    
     directory = 'images/'
     numbers_of_pictures = 30
     wiki_url = "https://upload.wikimedia.org/wikipedia/commons/3/3f/HST-SM4.jpeg"
     
     
-#    download_picture(directory, 'huble.jpeg', wiki_url)
-#    fetch_spacex_last_launch(directory)
-#    fetch_nasa_APOD(directory, numbers_of_pictures, nasa_token)
-#    fetch_nasa_EPIC(directory, nasa_token) 
-
-    publish_text_to_telegram(telegram_token, directory)
+    download_picture(directory, 'huble.jpeg', wiki_url)
+    fetch_spacex_last_launch(directory)
+    fetch_nasa_APOD(directory, numbers_of_pictures, nasa_token)
+    fetch_nasa_EPIC(directory, nasa_token) 
+    all_pictures = os.listdir(directory)
+    
+    for picture in all_pictures:
+        publish_text_to_telegram(telegram_token, directory, picture)
+        time.sleep(int(sleep_time))
